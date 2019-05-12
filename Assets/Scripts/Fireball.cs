2adf1ddae81a624fd46bd8a0aab707a8db6040a5
Óliver Máni <oliverhilmars@gmail.com>
2019-05-12 23:57:29 +0000

    Kommentaði kóðann

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -20,6 +20,7 @@ public class Fireball : MonoBehaviour
     public float speed;
 
 
+	// Þarf að nota þessi föll til að vinna með Sin Cos og Tan til að reikna stefnur og svona
     private static float DegreesToRadian(float deg){
         return (float)(Math.PI * deg / 180.0);
     }
@@ -37,8 +38,8 @@ public class Fireball : MonoBehaviour
             bulletTransform = GetComponent<Transform>();
             if(gameObject.tag == "PlayerBullet"){
                 angle = (float)DegreesToRadian(-player.transform.rotation.eulerAngles.z+90);
-                Vector2 velocity = player.rigid.velocity;
-                speed *= 1 + velocity.magnitude;
+                Vector2 velocity = player.rigid.velocity; //fær hraðann á þínu geimskipi (x og y)
+                speed *= 1 + velocity.magnitude; // geri þetta til að skotin glitch'i ekki þegar þú ert á hraðferð (fær hraðann á geimskipi sem float)
             } else if (gameObject.tag == "EnemyBullet"){
                 enemy = enemyController.clone;
                 float x = player.transform.position.x - enemy.transform.position.x,
@@ -47,15 +48,15 @@ public class Fireball : MonoBehaviour
 
                 
             }
-        } catch (UnassignedReferenceException e){}
+        } catch (UnassignedReferenceException e){} // til að leikurinn crashi ekki út af fyrsta skotinu í leiknum
        
     }
 
 
     void Update()
     {
-        Vector3 vec = new Vector3((float)Math.Cos(angle) * speed, (float)Math.Sin(angle) * speed,0);
-        bulletTransform.position -= vec;
+        Vector3 vec = new Vector3((float)Math.Cos(angle) * speed, (float)Math.Sin(angle) * speed,0); // lætur skotin stefna í rétta átt
+        bulletTransform.position -= vec; // hreyfir skotin
 
     }
 
